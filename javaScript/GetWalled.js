4bea6505b5fe7a7d5394cc37a4bb390f70599dc9
eb2511 <60404646+eb2511@users.noreply.github.com>
2024-05-13 20:19:33 -0400

    cors bypass?

--- javaScript/GetWalled.js
+++ javaScript/GetWalled.js
@@ -1,10 +1,13 @@
 function EatAWall(){
-    fetch('https://api.ipgeolocation.io/ipgeo?apiKey=c708e0003bba4906a4a189e3d26bc41f')
+    fetch('https://ipinfo.io?token=0126320920f585')
     .then(response => response.json())
     .then(data => {
-      if (data.country_code2 === 'CN') {
-        window.location.href = 'https://google.com'; // 重定向到一个“访问被阻止”的页面
-      }
+        if (data.country === 'CN') {
+            window.location.href = 'https://google.com';
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching geolocation data:', error);
     });
 }
 EatAWall()
