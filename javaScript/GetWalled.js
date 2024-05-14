0127775125b4f656a2f3063c0913f2d8bc63b493
eb2511 <60404646+eb2511@users.noreply.github.com>
2024-05-13 20:09:34 -0400

    again...

--- javaScript/GetWalled.js
+++ javaScript/GetWalled.js
@@ -1,11 +1,10 @@
-
-    fetch('https://api.ipgeolocation.io/ipgeo?apiKey=YOUR_API_KEY')
+function EatAWall(){
+    fetch('https://api.ipgeolocation.io/ipgeo?apiKey=c708e0003bba4906a4a189e3d26bc41f')
     .then(response => response.json())
     .then(data => {
       if (data.country_code2 === 'CN') {
-        window.location.href = 'https://example.com/blocked'; // 重定向到一个“访问被阻止”的页面
+        window.location.href = 'https://google.com'; // 重定向到一个“访问被阻止”的页面
       }
     });
-
-
-
+}
+EatAWall()
